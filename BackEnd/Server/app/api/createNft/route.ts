996314c2c6433c7bd9abed37735e130b078001dc
Marcus Arnett <marcodotio@Marcuss-MacBook-Pro-4.local>
2024-03-01 07:54:36 -0700

    Initial backend implementation

---
+++ BackEnd/Server/app/api/createNft/route.ts
@@ -0,0 +1,83 @@
+import { NextResponse } from 'next/server';
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { SolanaNftAnchor } from "../../../types/solana_nft_anchor";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import {
+	findMasterEditionPda,
+	findMetadataPda,
+	mplTokenMetadata,
+	MPL_TOKEN_METADATA_PROGRAM_ID,
+} from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { publicKey } from "@metaplex-foundation/umi";
+
+import {
+	TOKEN_PROGRAM_ID,
+	ASSOCIATED_TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+
+export async function POST(req: Request) {
+    const body = await req.json();
+    const { uri, name } = body;
+
+	const provider = anchor.AnchorProvider.env();
+	anchor.setProvider(provider);
+	const program = anchor.workspace
+		.SolanaNftAnchor as Program<SolanaNftAnchor>;
+
+	const signer = provider.wallet;
+
+	const umi = createUmi("https://api.devnet.solana.com")
+		.use(walletAdapterIdentity(signer))
+		.use(mplTokenMetadata());
+
+	const mint = anchor.web3.Keypair.generate();
+
+	const associatedTokenAccount = await getAssociatedTokenAddress(
+		mint.publicKey,
+		signer.publicKey
+	);
+
+	let metadataAccount = findMetadataPda(umi, {
+		mint: publicKey(mint.publicKey),
+	})[0];
+
+	let masterEditionAccount = findMasterEditionPda(umi, {
+		mint: publicKey(mint.publicKey),
+	})[0];
+
+	const metadata = {
+		name,
+		symbol: "PLT",
+		uri
+	};
+
+	const tx = await program.methods
+			.initNft(metadata.name, metadata.symbol, metadata.uri)
+			.accounts({
+				signer: provider.publicKey,
+				mint: mint.publicKey,
+				associatedTokenAccount,
+				metadataAccount,
+				masterEditionAccount,
+				tokenProgram: TOKEN_PROGRAM_ID,
+				associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+				tokenMetadataProgram: MPL_TOKEN_METADATA_PROGRAM_ID,
+				systemProgram: anchor.web3.SystemProgram.programId,
+				rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+			})
+			.signers([mint])
+			.rpc();
+	
+	return NextResponse.json(
+		{ transaction: tx },
+		{
+			status: 200,
+			headers: {
+				'content-type': 'text/plain',
+			},
+		},
+	);
+}
